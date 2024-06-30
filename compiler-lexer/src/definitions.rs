4467b17a257d487f158fda51902792256391140c
pedrodesu <me.pedro.ferreira@gmail.com>
2024-06-30 04:20:13 +0100

    made some optimisations to the lexer; error renamed and doesn't extend until EOF anymore

--- compiler-lexer/src/definitions.rs
+++ compiler-lexer/src/definitions.rs
@@ -7,8 +7,10 @@ const OPERATORS: &[&str] = &[
 
 const SEPARATORS: &[&str] = &["(", ")", "[", "]", "{", "}", ",", "."];
 
-const STR_DELIM: char = '"';
-const RUNE_DELIM: char = '`';
+pub const STR_DELIM: char = '"';
+pub const RUNE_DELIM: char = '`';
+pub const COMMENT_PREFIX: &str = "//";
+pub const COMMENT_PREFIX_LEN: usize = COMMENT_PREFIX.len();
 
 #[inline]
 pub(super) fn allow_type_transmutation(
@@ -18,7 +20,6 @@ pub(super) fn allow_type_transmutation(
     (curr_type == TokenType::Identifier && matches!(next_type, Some(TokenType::Keyword)))
         || (curr_type == TokenType::Literal(LiteralType::Int)
             && matches!(next_type, Some(TokenType::Literal(LiteralType::Float))))
-        || next == "//"
         || matches!(next, "0x" | "0o" | "0b")
 }
 
@@ -49,52 +50,52 @@ pub struct Token {
 
 impl TokenType {
     #[inline]
-    fn is_hex_int(expression: &str) -> bool {
-        if expression.len() <= 2 {
+    fn is_special_fmt_int(
+        expression: &str,
+        prefix: &str,
+        char_predicate: impl Fn(char) -> bool,
+    ) -> bool {
+        if expression.len() <= prefix.len() {
             false
         } else {
-            let (prefix, rem) = expression.split_at(2);
-            prefix == "0x"
-                && rem
-                    .chars()
-                    .all(|c| matches!(c, '0'..='9' | 'a'..='f' | 'A'..='F'))
+            expression
+                .split_at_checked(prefix.len())
+                .map(|e| e.0 == prefix && e.1.chars().all(char_predicate))
+                .unwrap_or_default()
         }
+    }
+
+    #[inline]
+    fn is_hex_int(expression: &str) -> bool {
+        Self::is_special_fmt_int(
+            expression,
+            "0x",
+            |c| matches!(c, '0'..='9' | 'a'..='f' | 'A'..='F'),
+        )
     }
 
     #[inline]
     fn is_dec_int(expression: &str) -> bool {
-        expression.chars().all(|c| c.is_ascii_digit() || c == '-')
-            && ((expression.matches('-').count() == 1 && expression.starts_with('-'))
-                || expression.matches('-').count() == 0)
+        expression.chars().all(|c| matches!(c, '0'..='9' | '-'))
+            && expression.match_indices('-').map(|v| v.0).sum::<usize>() == 0
     }
 
     #[inline]
     fn is_oct_int(expression: &str) -> bool {
-        if expression.len() <= 2 {
-            false
-        } else {
-            let (prefix, rem) = expression.split_at(2);
-            prefix == "0o" && rem.chars().all(|c| matches!(c, '0'..='7'))
-        }
+        Self::is_special_fmt_int(expression, "0o", |c| matches!(c, '0'..='7'))
     }
 
     #[inline]
     fn is_bin_int(expression: &str) -> bool {
-        if expression.len() <= 2 {
-            false
-        } else {
-            let (prefix, rem) = expression.split_at(2);
-            prefix == "0b" && rem.chars().all(|c| matches!(c, '0' | '1'))
-        }
+        Self::is_special_fmt_int(expression, "0b", |c| matches!(c, '0' | '1'))
     }
 
     #[inline]
     fn is_float(expression: &str) -> bool {
         expression
             .chars()
-            .all(|c| c.is_ascii_digit() || c == '-' || c == '.')
-            && ((expression.matches('-').count() == 1 && expression.starts_with('-'))
-                || expression.matches('-').count() == 0)
+            .all(|c| matches!(c, '0'..='9' | '-' | '.'))
+            && expression.match_indices('-').map(|v| v.0).sum::<usize>() == 0
             && (expression.matches('.').count() == 1 && !expression.starts_with('.'))
     }
 
@@ -107,16 +108,11 @@ impl TokenType {
     }
 
     #[inline]
-    pub fn eval(expr: &str, is_end_of_comment: bool) -> Option<Self> {
-        let is_delim =
-            |delim: char| expr.starts_with(delim) && expr.ends_with(delim) && expr.len() > 1;
-
+    pub fn eval(expr: &str) -> Option<Self> {
         if KEYWORDS.contains(&expr) {
             Some(TokenType::Keyword)
         } else if Self::is_identifier(expr) {
             Some(TokenType::Identifier)
-        } else if is_delim(RUNE_DELIM) {
-            Some(TokenType::Literal(LiteralType::Rune))
         } else if Self::is_hex_int(expr)
             || Self::is_dec_int(expr)
             || Self::is_oct_int(expr)
@@ -129,10 +125,6 @@ impl TokenType {
             Some(TokenType::Operator)
         } else if SEPARATORS.contains(&expr) {
             Some(TokenType::Separator)
-        } else if is_delim(STR_DELIM) {
-            Some(TokenType::Literal(LiteralType::String))
-        } else if expr.starts_with("//") && is_end_of_comment {
-            Some(TokenType::Comment)
         } else if expr == "\n" {
             Some(TokenType::Newline)
         } else {
--- compiler-lexer/src/lib.rs
+++ compiler-lexer/src/lib.rs
@@ -1,3 +1,6 @@
+#![feature(iter_next_chunk)]
+#![feature(let_chains)]
+
 use std::{iter::Peekable, mem, str::Chars};
 
 use anyhow::{anyhow, Result};
@@ -21,16 +24,45 @@ impl Iterator for Lexer<'_> {
             .peeking_take_while(|&c| c.is_ascii_whitespace() && c != '\n')
             .for_each(drop);
 
+        if let Some(&c) = self.iterator.peek()
+            && [definitions::STR_DELIM, definitions::RUNE_DELIM].contains(&c)
+        {
+            let Some(read) = self.iterator.clone().skip(1).position(|x| x == c) else {
+                return Some(Err(anyhow!("Unclosed {c}")));
+            };
+
+            return Some(Ok(Token {
+                value: self.iterator.by_ref().take(read + 2).collect(),
+                r#type: TokenType::Literal(if c == definitions::STR_DELIM {
+                    LiteralType::String
+                } else {
+                    LiteralType::Rune
+                }),
+            }));
+        }
+
+        if let Ok(rem) = self.iterator.clone().next_chunk::<COMMENT_PREFIX_LEN>()
+            && String::from_iter(rem) == COMMENT_PREFIX
+        {
+            let value = self
+                .iterator
+                .by_ref()
+                .peeking_take_while(|&c| c != '\n')
+                .collect();
+
+            return Some(Ok(Token {
+                value,
+                r#type: TokenType::Comment,
+            }));
+        }
+
         while let Some(c) = self.iterator.next() {
             acc.push(c);
 
-            if let Some(t) = TokenType::eval(
-                acc.as_str(),
-                matches!(self.iterator.peek(), None | Some('\n')),
-            ) {
+            if let Some(t) = TokenType::eval(acc.as_str()) {
                 if let Some(&next) = self.iterator.peek() {
                     let next_acc = acc.clone() + next.encode_utf8(&mut [0; 4]);
-                    let next_t = TokenType::eval(next_acc.as_str(), false); // end of comment doesn't matter on peek
+                    let next_t = TokenType::eval(next_acc.as_str());
 
                     if next_t
                         .is_some_and(|next_t| mem::discriminant(&t) == mem::discriminant(&next_t))
@@ -44,16 +76,15 @@ impl Iterator for Lexer<'_> {
                     value: acc,
                     r#type: t,
                 }));
+            } else if c.is_ascii_whitespace() {
+                return Some(Err(anyhow!(
+                    "`{:?}` is an invalid token",
+                    &acc[..acc.len() - 1]
+                )));
             }
         }
 
-        if !acc.is_empty() {
-            Some(Err(anyhow!(
-                "Incomplete expression ({acc:?} is an invalid expression)"
-            )))
-        } else {
-            None
-        }
+        None
     }
 }
 
